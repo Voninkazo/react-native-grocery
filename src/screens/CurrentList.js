0f283e25ec897ad536164c7d2df736f042d1e576
Sandy <sandy.rav@onja.org>
2021-04-15 14:38:25 +0300

    got adding item working

--- src/screens/CurrentList.js
+++ src/screens/CurrentList.js
@@ -1,25 +1,44 @@
-import React from 'react'
+import React, { useState } from 'react';
+import {v4 as uuid} from 'uuid';
 
-import { View, Text, SafeAreaView, FlatList } from 'react-native'
+import { View, Text, SafeAreaView, FlatList, KeyboardAvoidingView } from 'react-native'
 
 import nachos from '../data/nachos'
 import ListItem, { Separator } from '../components/ListItem';
+import AddItem from '../components/AddItem';
 
 export default () => {
+    const [list, setList] = useState(nachos);
     return (
-        <SafeAreaView>
-         <FlatList
-         data = {nachos}
-        renderItem={({item, index}) => (
-                   <ListItem 
-                    name={item.name}
-                    onFavoritePress={() => alert('todo: handle favorite')}
-                    isFavorite={index < 2}
-                />
-        )}
-        keyExtractor={(item) => item.id}
-        ItemSeparatorComponent = {() => <Separator />}
+        <SafeAreaView  style={{flex: 1}}>
+            <KeyboardAvoidingView
+             style={{flex: 1}}
+             behavior='padding'
+            >
+            <FlatList
+                data = {list}
+                renderItem={({item, index}) => (
+                    <ListItem 
+                        name={item.name}
+                        onFavoritePress={() => alert('todo: handle favorite')}
+                        isFavorite={index < 2}
+                    />
+                )}
+                keyExtractor={(item) => item.id}
+                ItemSeparatorComponent = {() => <Separator />}
+                ListHeaderComponent={() => {
+                    return (
+                        <AddItem 
+                        onSubmitEditing={({nativeEvent: {text}}) => {
+                            setList([{id:uuid(),name: text},...list])
+                        }}
+                        />
+                    )
+                }
+            }
+
           />
+          </KeyboardAvoidingView>
         </SafeAreaView>
     )
 };
