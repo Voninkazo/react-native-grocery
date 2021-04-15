53d7f1f055dc0ff8d85ab81313561bb13b2f0dc6
Sandy <sandy.rav@onja.org>
2021-04-15 11:31:34 +0300

    midified code to make it simple

--- src/screens/CurrentList.js
+++ src/screens/CurrentList.js
@@ -1,26 +1,25 @@
 import React from 'react'
 
-import { View, Text, SafeAreaView, ScrollView } from 'react-native'
+import { View, Text, SafeAreaView, FlatList } from 'react-native'
 
 import nachos from '../data/nachos'
-import ListItem ,{Separator}from '../components/ListItem';
+import ListItem, { Separator } from '../components/ListItem';
 
 export default () => {
     return (
         <SafeAreaView>
-           <ScrollView>
-           {nachos.map((item, index) => (
-               <React.Fragment  key={item.id}>
+         <FlatList
+         data = {nachos}
+        renderItem={({item, index}) => (
                    <ListItem 
                     name={item.name}
                     onFavoritePress={() => alert('todo: handle favorite')}
                     isFavorite={index < 2}
                 />
-                   <Separator />
-               </React.Fragment>    
-           )
-           )}
-           </ScrollView>
+        )}
+        keyExtractor={(item) => item.id}
+        ItemSeparatorComponent = {() => <Separator />}
+          />
         </SafeAreaView>
     )
 };
