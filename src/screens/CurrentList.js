2249883db07e986100b0181d2fad2854fe64baf3
Sandy <sandy.rav@onja.org>
2021-04-16 11:57:04 +0300

    Got list management working

--- src/screens/CurrentList.js
+++ src/screens/CurrentList.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
-import {v4 as uuid} from 'uuid';
-import AsyncStorage from '@react-native-community/async-storage';
+//import {v4 as uuid} from 'uuid';
+//import AsyncStorage from '@react-native-community/async-storage';
 
 import { View, Text, SafeAreaView, FlatList, KeyboardAvoidingView, ActivityIndicator } from 'react-native'
 
@@ -8,38 +8,42 @@ import { View, Text, SafeAreaView, FlatList, KeyboardAvoidingView, ActivityIndic
 import ListItem, { Separator } from '../components/ListItem';
 import AddItem from '../components/AddItem';
 
-const updateStorageCurrentList = (list) => {
-    AsyncStorage.setItem('@@GroceryList/currentList', JSON.stringify(list))
-}
+// const updateStorageCurrentList = (list) => {
+//     AsyncStorage.setItem('@@GroceryList/currentList', JSON.stringify(list))
+// }
 
+    import {useCurrentList} from '../util/ListManager';
 
 export default () => {
-    const [list, setList] = useState([]);
-    const [loading, setLoading] = useState(true);
+    const {list,loading, addItem, removeItem} = useCurrentList();
 
-    const addItem = (text) => {
-        const newList =  [{id:uuid(),name: text},...list];
-        setList(newList);
-        updateStorageCurrentList(newList);
-    }
 
-    const removeItem = (id) => {
-        const newList = list.filter(item => item.id !== id);
-        setList(newList)
-        updateStorageCurrentList(newList);
-    }
+    // const [list, setList] = useState([]);
+    // const [loading, setLoading] = useState(true);
 
-    useEffect(() => {
-      AsyncStorage.getItem('@@GroceryList/currentList')
-      .then(data => JSON.parse(data))
-      .then(data => {
-          if(data) {
-              setList(data);
-          }
-          setLoading(false);
-      })
-      1000
-    }, [])
+    // const addItem = (text) => {
+    //     const newList =  [{id:uuid(),name: text},...list];
+    //     setList(newList);
+    //     updateStorageCurrentList(newList);
+    // }
+
+    // const removeItem = (id) => {
+    //     const newList = list.filter(item => item.id !== id);
+    //     setList(newList)
+    //     updateStorageCurrentList(newList);
+    // }
+
+    // useEffect(() => {
+    //   AsyncStorage.getItem('@@GroceryList/currentList')
+    //   .then(data => JSON.parse(data))
+    //   .then(data => {
+    //       if(data) {
+    //           setList(data);
+    //       }
+    //       setLoading(false);
+    //   })
+    //   1000
+    // }, [])
 
     if (loading) {
         return (
