93c6cd35ee2683226cb0892388e9b50a17ac5df9
Sandy <sandy.rav@onja.org>
2021-04-16 11:29:06 +0300

    Got local storage working to store data

--- src/screens/CurrentList.js
+++ src/screens/CurrentList.js
@@ -1,14 +1,54 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import {v4 as uuid} from 'uuid';
+import AsyncStorage from '@react-native-community/async-storage';
 
-import { View, Text, SafeAreaView, FlatList, KeyboardAvoidingView } from 'react-native'
+import { View, Text, SafeAreaView, FlatList, KeyboardAvoidingView, ActivityIndicator } from 'react-native'
 
-import nachos from '../data/nachos'
+//import nachos from '../data/nachos'
 import ListItem, { Separator } from '../components/ListItem';
 import AddItem from '../components/AddItem';
 
+const updateStorageCurrentList = (list) => {
+    AsyncStorage.setItem('@@GroceryList/currentList', JSON.stringify(list))
+}
+
+
 export default () => {
-    const [list, setList] = useState(nachos);
+    const [list, setList] = useState([]);
+    const [loading, setLoading] = useState(true);
+
+    const addItem = (text) => {
+        const newList =  [{id:uuid(),name: text},...list];
+        setList(newList);
+        updateStorageCurrentList(newList);
+    }
+
+    const removeItem = (id) => {
+        const newList = list.filter(item => item.id !== id);
+        setList(newList)
+        updateStorageCurrentList(newList);
+    }
+
+    useEffect(() => {
+      AsyncStorage.getItem('@@GroceryList/currentList')
+      .then(data => JSON.parse(data))
+      .then(data => {
+          if(data) {
+              setList(data);
+          }
+          setLoading(false);
+      })
+      1000
+    }, [])
+
+    if (loading) {
+        return (
+            <SafeAreaView>
+               <Text>Loading...</Text>
+            </SafeAreaView>
+        )
+    }
+
     return (
         <SafeAreaView  style={{flex: 1}}>
             <KeyboardAvoidingView
@@ -22,8 +62,8 @@ export default () => {
                         name={item.name}
                         onFavoritePress={() => alert('todo: handle favorite')}
                         isFavorite={index < 2}
-                        onAddedSwipe={() => alert('todo: on added swipe')}
-                        onDeleteSwipe={() => alert('todo: on delete swipe')}
+                        onAddedSwipe={() => removeItem(item.id)}
+                        onDeleteSwipe={() => removeItem(item.id)}
                     />
                 )}
                 keyExtractor={(item) => item.id}
@@ -31,9 +71,8 @@ export default () => {
                 ListHeaderComponent={() => {
                     return (
                         <AddItem 
-                        onSubmitEditing={({nativeEvent: {text}}) => {
-                            setList([{id:uuid(),name: text},...list])
-                        }}
+                        onSubmitEditing={({nativeEvent: {text}}) => addItem(text)
+                        }
                         />
                     )
                 }
