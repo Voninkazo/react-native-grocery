34b186eb159f4d083163b703f3d53e505628c280
Sandy <sandy.rav@onja.org>
2021-04-19 12:02:06 +0300

    got switching between two pages working with focused

--- src/navigation/index.js
+++ src/navigation/index.js
@@ -1,15 +1,18 @@
 import React from 'react';
-import {Text} from 'react-native';
+import {Text, Image, Platform} from 'react-native';
 import CurrentList from '../screens/CurrentList';
 import {createStackNavigator} from '@react-navigation/stack';
 import {NavigationContainer} from '@react-navigation/native';
 import ItemDetails from '../screens/ItemDetails';
+import FavoriteList from '../screens/FavoriteList';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+
+const Tab = createBottomTabNavigator();
 
 const Stack = createStackNavigator();
 
 const CurrentListStack = () => {
     return (
-        <NavigationContainer>
             <Stack.Navigator>
                 <Stack.Screen name="CurrentList" component={CurrentList} />
                 <Stack.Screen name="ItemDetails" component={ItemDetails} 
@@ -23,11 +26,54 @@ const CurrentListStack = () => {
                 />
               
             </Stack.Navigator>
+    )
+}
+
+const FavouritListStack = () => {
+    return (
+        <Stack.Navigator>
+            <Stack.Screen name="FavoritList" component={FavoriteList} />
+        </Stack.Navigator>
+    )
+}
+
+const Tabs = () => {
+    return (
+        <NavigationContainer>
+             <Tab.Navigator
+             screenOptions={({route}) => ({
+                 tabBarIcon: ({color, focused}) => {
+                     let image;
+                     const routeName = route.name;
+
+                     if(routeName === 'CurrentList') {
+                         image = Platform.select({
+                             ios: require('../assets/icons/ios-list.png'),
+                             android: require('../assets/icons/md-list.png')
+                         });
+                     }
+                     else if(routeName === 'FavoriteList') {
+                         image = Platform.select({
+                             ios: focused ? require('../assets/icons/ios-star.png')
+                             : require('../assets/icons/ios-star-outline.png'),
+                             android: focused ? require('../assets/icons/md-star.png')
+                             : require('../assets/icons/md-star-outline.png'),
+                         });
+                     }
+                     return <Image source={image}
+                     resizeMode='contain'
+                     style = {{width: 25, tintColor: color}} />
+                 }
+             })}
+             >
+                <Tab.Screen name="FavoriteList" component={FavouritListStack} />
+                <Tab.Screen name="CurrentList" component={CurrentListStack} />
+            </Tab.Navigator>
         </NavigationContainer>
     )
 }
 
-export default CurrentListStack;
+export default Tabs;
 
 
 // import { createStackNavigator, createAppNavigator } from 'react-navigation';
